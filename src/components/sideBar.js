c618343f578fd1f89386641939d81f58c8c36ac3
RyanVreeke <rvreeke0229@gmail.com>
2021-03-20 18:00:30 -0400

    Added a link to my FreeTime app GitHub page.

--- src/components/sideBar.js
+++ src/components/sideBar.js
@@ -10,6 +10,8 @@ export default function SideBar() {
         <div>
         <text style={{fontSize: '15px'}}>rvreeke0229@gmail.com</text>
         </div>
+        <a href="https://github.com/calvin-cs262-fall2020-teamD/freetime-project" className="Link">FreeTime App</a>
+        <br></br>
         <a href="https://trivia336.web.app/" className="Link">Trivia App</a>
       </div>
     )
